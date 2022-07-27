2659efb466c2edf0180f5c6f0a61576f06ebdb7e
Val-Belous <vaaalbelous@gmail.com>
2022-07-27 16:15:27 +0300

    spiner in da centru

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,12 +3,16 @@ import { Triangle } from 'react-loader-spinner';
 const Loader = () => {
   return (
     <Triangle
-      height="80"
-      width="80"
+      height="180"
+      width="180"
       radius="9"
       color="green"
       ariaLabel="three-dots-loading"
-      wrapperStyle
+      wrapperStyle={{
+        dispay: 'flex',
+        justifyContent: 'center',
+        marginTop: 200,
+      }}
       wrapperClass
     />
   );
